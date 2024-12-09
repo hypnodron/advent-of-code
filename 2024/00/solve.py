9fe3c9dd5d8bf8538e6efb75d5c24f246bd0ded5
hypnodron <hypnodron@gmail.com>
2024-12-09 20:56:56 +0500

    Update template

--- 2024/00/solve.py
+++ 2024/00/solve.py
@@ -1,28 +1,28 @@
 def read_input(file_path: str) -> list[str]:
     with open(file_path, "r") as file:
-        return file.readlines()
+        return [line.strip() for line in file.readlines()]
 
 
-def solve1(input_lines: list[str]) -> str:
-    input = [line.strip() for line in input_lines]
+def solve1(input: list[str]) -> str:
+    pass
 
 
-def solve2(input_lines: list[str]) -> str:
-    input = [line.strip() for line in input_lines]
+def solve2(input: list[str]) -> str:
+    pass
 
 
 def main():
-    example_input_lines = read_input("input-example.txt")
-    puzzle_input_lines = read_input("input-puzzle.txt")
+    example_input = read_input("input-example.txt")
+    puzzle_input = read_input("input-puzzle.txt")
     # part 1
-    example_output1 = solve1(example_input_lines)
+    example_output1 = solve1(example_input)
     print(f"Example output 1:\n{example_output1}\n")
-    puzzle_output1 = solve1(puzzle_input_lines)
+    puzzle_output1 = solve1(puzzle_input)
     print(f"Puzzle output 1:\n{puzzle_output1}\n")
     # part 2
-    example_output2 = solve2(example_input_lines)
+    example_output2 = solve2(example_input)
     print(f"Example output 2:\n{example_output2}\n")
-    puzzle_output2 = solve2(puzzle_input_lines)
+    puzzle_output2 = solve2(puzzle_input)
     print(f"Puzzle output 2:\n{puzzle_output2}")
 
 
